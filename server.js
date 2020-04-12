7d795542d40fbc328070cfc2a2407574bdff92cf
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-04-12 22:25:56 +0300

    book-app

--- server.js
+++ server.js
@@ -10,37 +10,69 @@ const superagent = require('superagent');
 app.use(express.static('./public'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('pages/index');
-    // res.status(200).send('okkkkkkkkk');
 })
 
-// app.get('/list',(req,res)=>{
-//     let family = ['Atallah','Mesina','Ali','Eman'];
-//     res.render('listFamily',{data:family});
-// })
 
-// app.get('/books',(req,res)=>{
-//     // get books from google book api
-//     // .then
-//     // send the result data to render them by res.render
 
-//     let url =`https://www.googleapis.com/books/v1/volumes?q=cats`;
-//     superagent.get(url)
-//     .then(data => {
-//         // res.json(data.body);
-//         res.render('booksPage',{book:data.body.items})
-//     })
-// })
+app.post('/searches', (req, res) => {
+    var url;
+    if (req.body.searchtype === 'title') {
+        url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}&intitle=${req.body.bookname}`;
+        // url = url + req.body.searchtype;
+        console.log('asdsad', url);
+    }
+    else if (req.body.searchtype === 'author') {
+    console.log('sadads','authorrrrrrrrrrrr');
+        url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}+inauthor:${req.body.bookname}`;
+        // url = url + req.body.searchtype;
+        console.log('asdsad', url);
+    }
+    else
+    {
+        
+    }
 
-app.get('*',(req,res)=>{
+    superagent.get(url)
+        .then(data => {
+            console.log('alaa');
+
+            let arr = data.body.items;
+
+            let books = arr.map(book => {
+               
+                    let alaa = new Book(book);
+                    console.log(book.length);
+                    return alaa;
+                
+               
+                
+
+            });
+            res.render('pages/searches/show', { books: books })
+        })
+        .catch(error => {
+            res.render('pages/error');
+        });
+});
+
+
+function Book(book) {
+    this.title = book.volumeInfo.title;
+    this.smallThumbnail=book.volumeInfo.imageLinks.smallThumbnail;
+    this.authors=book.volumeInfo.authors;
+    this.description=book.volumeInfo.description;
+
+}
+app.get('*', (req, res) => {
     res.status(404).send('This route does not exist!!');
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
