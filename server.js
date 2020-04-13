cd0b4503291a24b30ca5a0dee86e03e5e14992ab
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-04-14 01:17:38 +0300

    lab12

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-
 const PORT = process.env.PORT || 3030;
 const app = express();
 const superagent = require('superagent');
-
+const pg  = require('pg');
 app.use(express.static('./public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+const client = new pg.Client(process.env.DATABASE_URL);
 app.set('view engine', 'ejs');
+var bookgallery=[];
 
-app.get('/', (req, res) => {
-    res.render('pages/index');
+app.get('/',(req,res)=>{
+res.redirect('/index')
 })
+app.get('/index', (req, res) => {
+    let SQL = 'SELECT * FROM books;';
+    return client.query(SQL)
+    .then(results =>{
+        res.render('pages/index',{books:results.rows});
+    })
+})
+
 app.get('/search', (req, res) => {
     res.render('pages/searches/new')
 })
 
+
 app.post('/searches', (req, res) => {
+
     var url;
     if (req.body.searchtype === 'title') {
         url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.bookname}&intitle:${req.body.bookname}`;
@@ -34,6 +43,7 @@ app.post('/searches', (req, res) => {
             let arr = data.body.items;
             let books = arr.map(book => {
                 let bookitem = new Book(book);
+                bookgallery.push(bookitem);
                 console.log(book.length);
                 return bookitem;
             });
@@ -43,17 +53,44 @@ app.post('/searches', (req, res) => {
             res.render('pages/error');
         });
 });
+
+app.get('/books/:book_id', getbookDetails);
+function getbookDetails(req,res) {
+    let SQL = 'SELECT * FROM books WHERE id=$1;';
+    let values = [req.params.book_id];
+    return client.query(SQL,values)
+    .then (result =>{
+        
+        res.render('pages/book/detail',{details:result.rows[0]});
+    })
+}
+
+app.post('/books', insertDetails);
+
+function insertDetails(req,res) {
+    let {title,author,isbn,image_url,description} = req.body;
+    let SQL = 'INSERT INTO books (title,author,isbn,image_url,description) VALUES ($1,$2,$3,$4,$5);';
+    let safeValues = [title,author,isbn,image_url,description];
+    return client.query(SQL,safeValues)
+    .then (()=>{
+        res.redirect('/index');
+    })
+        
+ }
 function Book(book) {
     this.title = book.volumeInfo.title;
     this.smallThumbnail = book.volumeInfo.imageLinks.smallThumbnail;
     this.authors = book.volumeInfo.authors;
     this.description = book.volumeInfo.description;
-
+    this.isbn=book.volumeInfo.industryIdentifiers[0].type;
 }
 app.get('*', (req, res) => {
     res.status(404).send('This route does not exist!!');
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
+client.connect()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Listening on PORT ${PORT}`);
+    })
 })
